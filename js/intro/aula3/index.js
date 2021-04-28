c68d508d89747112d5432ed09b1a793b01e2e083
Josue Gomes Ribeiro <josuex.x.x@hotmail.com>
2021-04-28 09:59:17 -0300

    conceitos js v1

---
+++ js/intro/aula3/index.js
@@ -0,0 +1,28 @@
+/*let nome = 'josh';
+//or var
+let qualquer; // Declaraçao sem uso
+
+//console.log(nome); 
+console.log(12345,"wow" );
+console.log('Meu nome e', nome,'. Estou aprednedo JavaScript as 16 da tarde.') 
+console.log('ola mundo')
+
+const valorUm = 5;
+const valorDois = 5;
+const valorSoma = valorUm + valorDois;
+console.log(valorSoma);
+const valorMulti = valorSoma * 3;
+console.log(valorMulti);
+*/
+
+const nome ='Josue';
+const sobrenome = 'gomes ribeiro';
+const idade = 29;
+const peso = 64.5;
+const altura = 1.73;
+let imc;
+imc = peso / (altura * altura)
+let anoNascimento;
+anoNascimento = (2021 - idade) -1 ;
+
+console.log(`${nome} ${sobrenome}, nascido em: ${anoNascimento}, tem: ${idade} anos e pesa: ${imc}`);
