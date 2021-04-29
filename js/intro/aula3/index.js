c8c0eac99a561a0854c9600a85a20ec66d9f0145
Josue Gomes Ribeiro <josuex.x.x@hotmail.com>
2021-04-29 10:41:13 -0300

    conceitos js v2

--- js/intro/aula3/index.js
+++ js/intro/aula3/index.js
@@ -15,7 +15,7 @@ const valorMulti = valorSoma * 3;
 console.log(valorMulti);
 */
 
-const nome ='Josue';
+/*const nome ='Josue';
 const sobrenome = 'gomes ribeiro';
 const idade = 29;
 const peso = 64.5;
@@ -26,3 +26,44 @@ let anoNascimento;
 anoNascimento = (2021 - idade) -1 ;
 
 console.log(`${nome} ${sobrenome}, nascido em: ${anoNascimento}, tem: ${idade} anos e pesa: ${imc}`);
+
+*/
+
+/*let num1 = prompt('Digite um numero');
+let num2 = prompt('Digite outro numero');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+//let resultado = num1 + num2;
+
+alert(`o resultado da sua conta e: + ${num1 + num2}`);
+
+*/
+
+/*let varA = 'A'; //b
+let varB = 'B'; //c
+let varC = 'C'; //a
+console.log(varA,varB,varC);
+
+let b = varB;
+let c = varC;
+let a = varA
+
+varA = b;
+varB = c;
+varC = a;
+
+console.log(varA,varB,varC);
+*/
+
+const nome = prompt('Digite seu nome completo:');
+document.body.innerHTML += `O seu nome é: ${nome}<br/>`;
+document.body.innerHTML += `Seu nome tem ${nome.length} letras <br />`;
+document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]} <br />`;
+document.body.innerHTML += `Qual o primeiro índice da letra A no seu nome? ${nome.indexOf("a")} <br />`;
+document.body.innerHTML += `Qual o último índice da letra LETRA no seu nome? ${nome.slice(-1)}<br />`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(-3)}<br />`;
+document.body.innerHTML += `As palavras do seu nome são: ${nome.split(' ')}<br />`;
+document.body.innerHTML += `Seu nome com letras maiúsculas: ${nome.toUpperCase()}<br />`;
+document.body.innerHTML += `Seu nome com letras minúsculas: ${nome.toLowerCase()}<br />`;
