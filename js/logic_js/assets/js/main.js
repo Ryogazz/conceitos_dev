896f1ee538839ecdf1950f4878d7745c95a8c5b0
Josue Gomes Ribeiro <josuex.x.x@hotmail.com>
2021-04-30 17:07:31 -0300

    logica js v1

---
+++ js/logic_js/assets/js/main.js
@@ -0,0 +1,22 @@
+const tenhoGrana = NaN;
+
+if (tenhoGrana) {
+    console.log('Vou sair de casa');
+}
+else{
+    console.log('Não vou sair de casa');
+}
+
+const numero = NaN;
+if (numero >= 0 && numero <= 5){
+    console.log('o numero está entre o 0 e 5');
+}
+else if (numero >=6 && numero <= 8){
+    console.log('o numero esta entre 6 e 8');
+}
+else if (numero >=9 && numero <= 11){
+    console.log('o numero esta entre 9 e 11');
+}
+else{
+    console.log('O numero não está entre 0 e 11.')
+}
