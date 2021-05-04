9048d27c19b92b39b25eebfd2d8a3602e8b21ba8
Josue Gomes Ribeiro <josuex.x.x@hotmail.com>
2021-05-04 18:23:58 -0300

    logica js v2

--- js/logic_js/assets/js/main.js
+++ js/logic_js/assets/js/main.js
@@ -1,22 +1,64 @@
-const tenhoGrana = NaN;
+const form = document.querySelector('#formulario');
 
-if (tenhoGrana) {
-    console.log('Vou sair de casa');
-}
-else{
-    console.log('Não vou sair de casa');
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+    const inputPeso = event.target.querySelector('#peso');
+    const inputAltura = event.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso){
+        setResultado('Peso invalido', false);
+        return;
+    }
+    if (!altura){
+        setResultado('Altura invalida ', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    const mensagem = `Seu IMC é ${imc} (${nivelImc}).`;
+
+    setResultado(mensagem, true);
+
+});
+
+function criaP(){
+    const p = document.createElement('p');
+    return p;
 }
 
-const numero = NaN;
-if (numero >= 0 && numero <= 5){
-    console.log('o numero está entre o 0 e 5');
+function setResultado(msg, isValid){
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';  
+    const p = criaP();
+
+    if(isValid) {
+        p.classList.add('paragrafo-resultado')
+    }
+    else{
+        p.classList.add('bad')
+    }
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+};
+
+function getImc(peso, altura){
+    const imc = peso / altura **2;
+    return imc.toFixed(2);
 }
-else if (numero >=6 && numero <= 8){
-    console.log('o numero esta entre 6 e 8');
-}
-else if (numero >=9 && numero <= 11){
-    console.log('o numero esta entre 9 e 11');
-}
-else{
-    console.log('O numero não está entre 0 e 11.')
+
+function getNivelImc(imc){
+    const nivel = ['Abaixo do peso','Peso normal','Sobrepeso','Obesidade grau 1',
+    'Obesidade grau 2', 'Obesidade grau 3'];
+
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
 }
