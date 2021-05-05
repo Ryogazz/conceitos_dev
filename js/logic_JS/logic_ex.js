6920d7fc1bf694cc1d4a68336e2051a2494588a7
Josue Gomes Ribeiro <josuex.x.x@hotmail.com>
2021-05-05 19:43:38 -0300

    logica js v3

---
+++ js/logic_JS/logic_ex.js
@@ -0,0 +1,8 @@
+//condiçao ternaria
+const pontosUsuario = 1000;
+const nivelUsuario = pontosUsuario >= 1000 ? 'Usuario VIP' : 'Usuario normal';
+console.log(nivelUsuario);
+
+//object date
+const data = new Date(0);
+console.log(data.toDateString());
